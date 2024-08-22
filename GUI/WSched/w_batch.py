fdc023df9a55ba90b348d03dc933460efe9cd24f
G4GA <damian.gaga02@gmail.com>
2024-08-21 23:01:26 -0600

    a

--- GUI/WSched/w_batch.py
+++ GUI/WSched/w_batch.py
@@ -1,7 +1,15 @@
-from .w_sched import WinScheduler
+from .w_sched import SchedWindow
+from PyQt6.QtWidgets import (
+    QLabel
+)
 
-class BatchWindow(WinScheduler):
+class BatchWindow(SchedWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Batch Processing")
+
+        self.test_label = QLabel('Testing')
+        self.main_layout.addWidget(self.test_label)
+
+        self.main_widget.setStyleSheet('background-color: blue')
--- GUI/WSched/w_sched.py
+++ GUI/WSched/w_sched.py
@@ -1,19 +1,26 @@
-from PyQt6.QtWidgets import {
+from PyQt6.QtWidgets import (
     QMainWindow,
     QWidget,
+    QVBoxLayout,
+)
 
-}
-
-class WinScheduler(QMainWindow):
+class SchedWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.__components = {
-            'upper': {
-                'widget': 
-            },
-            'lower': {}
+            'main_widget': QWidget(),
+            'main_layout': QVBoxLayout()
         }
 
-    def init_window(self):
-        pass
+    @property
+    def main_widget(self):
+        return self.__components['main_widget']
+
+    @property
+    def main_layout(self):
+        return self.__components['main_layout']
+
+    def _init_window(self):
+        self.main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.main_widget)
 
